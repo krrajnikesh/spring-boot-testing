9917d4c898f6296ef2a31e39d0ec8199d916a8b4
krrajnikesh <krrajnikesh@gmail.com>
2025-01-16 22:28:57 +0530

    JUnit test to find object by using JPQL query with named params

--- src/main/java/net/java/junit/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/java/junit/springboot/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package net.java.junit.springboot.repository;
 import net.java.junit.springboot.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,4 +14,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //Define custom query using JPQL with index parameter
     @Query("select e from Employee e where e.firstName = ?1 and e.lastName = ?2")
     Employee findByJPQL(String firstName, String lastName);
+
+    //Define custom query using JPQL with named parameter
+    @Query("select e from Employee e where e.firstName =:firstName and e.lastName =:lastName")
+    Employee findByNamedParams(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
