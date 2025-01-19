1c907feda1980744f7680b7b877427caff9c5013
krrajnikesh <krrajnikesh@gmail.com>
2025-01-19 18:56:24 +0530

    created test case for save employee method

--- src/test/java/net/java/junit/springboot/SpringBootTestingApplicationTests.java
+++ src/test/java/net/java/junit/springboot/SpringBootTestingApplicationTests.java
@@ -8,6 +8,7 @@ class SpringBootTestingApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
--- src/test/java/net/java/junit/springboot/service/EmployeeServiceTests.java
+++ src/test/java/net/java/junit/springboot/service/EmployeeServiceTests.java
@@ -1,9 +1,16 @@
 package net.java.junit.springboot.service;
 
+import net.java.junit.springboot.model.Employee;
 import net.java.junit.springboot.repository.EmployeeRepository;
 import net.java.junit.springboot.service.impl.EmployeeServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+
+import java.util.Optional;
 
 public class EmployeeServiceTests {
 
@@ -14,6 +21,30 @@ public class EmployeeServiceTests {
     void setUp(){
         employeeRepository = Mockito.mock(EmployeeRepository.class);
         employeeService = new EmployeeServiceImpl(employeeRepository);
+    }
+
+    //JUnit test for saveEmployee method
+    @DisplayName("JUnit test for saveEmployee method")
+    @Test
+    void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject(){
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("Ram")
+                .id(1L)
+                .lastName("Kumar")
+                .email("ram@gmail.com")
+                .build();
+
+        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
+
+        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+
+        //when - action or the behaviour that are going to be tested
+        Employee savedEmployee = employeeService.saveEmployee(employee);
+
+        //then - verify the output
+        Assertions.assertThat(savedEmployee).isNotNull();
+
     }
 
 }
